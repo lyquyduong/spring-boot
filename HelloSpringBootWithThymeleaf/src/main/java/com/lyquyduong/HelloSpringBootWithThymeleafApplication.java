a090706fc3984492379dd3de63fe142d2c72f92a
Ly Quy duong <lyquyduong@gmail.com>
2017-09-07 12:57:24 +0700

    init

---
+++ HelloSpringBootWithThymeleaf/src/main/java/com/lyquyduong/HelloSpringBootWithThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.lyquyduong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootWithThymeleafApplication.class, args);
+	}
+}
